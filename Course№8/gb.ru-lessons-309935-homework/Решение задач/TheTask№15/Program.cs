96d9d00667b69ff654e789a96763bb15ba40749f
DANIIL KONEF <daniilkonef@yandex.ru>
2023-04-03 00:33:43 +0300

    Успешно готова Задача 15 (обычный уровень сложности)

--- Course№8/gb.ru-lessons-309935-homework/Решение задач/TheTask№15/Program.cs
+++ Course№8/gb.ru-lessons-309935-homework/Решение задач/TheTask№15/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+    Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+    6 -> да
+    7 -> да
+    1 -> нет
+    * Сделать вариант с использованием конструкции Dictionary
+*/
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +21,54 @@ namespace TheTask_15
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("введите номер дня недели от 1 до 7: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                
+                if(Equals(n,1)) 
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: ПОНЕДЕЛЬНИК");
+                }
+
+                if (Equals(n, 2))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: ВТОРНИК");
+                }
+
+                if (Equals(n, 3))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: СРЕДА");
+                }
+
+                if (Equals(n, 4))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: ЧЕТВЕРГ");
+                }
+
+                if (Equals(n, 5))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: ПЯТНИЦА");
+                }
+
+                if (Equals(n, 6))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: СУББОТА");
+                }
+
+                if (Equals(n, 7))
+                {
+                    Console.WriteLine($"Цифра {n} соответствует дню недели: ВОСКРЕСЕНЬЕ");
+                }
+
+                if (n<1 || n>7)
+                {
+                    Console.WriteLine($"Цифра {n} не соответствует никакому дню недели");
+                }
+
+                Console.WriteLine();
+
+            }
         }
     }
 }
