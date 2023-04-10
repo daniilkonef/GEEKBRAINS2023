3ff921af7d919eb60a61126cbba8a4e9f012dfff
DANIIL KONEF <daniilkonef@yandex.ru>
2023-04-10 23:54:37 +0300

    Начал решать задачу 27

---
+++ Course8/gb.ru-lessons-309937-homework/Mission27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+    https://gb.ru/lessons/309937/homework    
+    Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+    пример: 452 -> 11,
+    пример: 82 -> 10,
+    пример: 9012 -> 12.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
