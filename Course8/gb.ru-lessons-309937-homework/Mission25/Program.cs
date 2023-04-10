7120321f5342b6dedb00dc9f3622e6849dfda31b
DANIIL KONEF <daniilkonef@yandex.ru>
2023-04-10 23:48:50 +0300

    Готова задача 25

---
+++ Course8/gb.ru-lessons-309937-homework/Mission25/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+    https://gb.ru/lessons/309937/homework
+    Знакомство с языками программирования (семинары) - Урок 4. Функции
+    Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+    3, 5 -> 243 (3⁵)
+    2, 4 -> 16
+ */
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+
+                Int32 A = 0;
+                Int32 B = 0;
+                
+                
+
+                Console.Write("Введите число A: "); // вывожу приглашение пользователю 
+                A = Convert.ToInt32(Console.ReadLine()); // читаю текстовый ввод пользователя и преобразовываю его в целое число 
+
+                Console.Write("Введите число B: "); // вывожу приглашение пользователю 
+                B = Convert.ToInt32(Console.ReadLine()); // читаю текстовый ввод пользователя и преобразовываю его в целое число 
+
+                Int32 C = A;
+                for (int i = 2; 
+                     i <= B; 
+                     i++)
+                {
+                    C = C*A;
+                }
+
+                Console.WriteLine(C);
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
