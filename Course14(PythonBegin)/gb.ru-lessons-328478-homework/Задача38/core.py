9f86f895c9287715138583bd7b9f888d70b3ffbd
DANIILKONEF <daniilkonef@yandex.ru>
2023-06-24 18:05:57 +0300

    Надо поработать над удалением

--- Course14(PythonBegin)/gb.ru-lessons-328478-homework/Задача38/core.py
+++ Course14(PythonBegin)/gb.ru-lessons-328478-homework/Задача38/core.py
@@ -3,13 +3,15 @@
 #             '3;Ингви;Джей;Мальмстин;+198652338',
 #             '4;Брайян;Джонсон;ACDC;+198652338',
 #             '5;Джо;Ди;Майо;+16916988484']
-#
-# 
+# #
+# #
 # print(len(database))
 
+
 def find_record_in(database, what_im_looking_for: str):
-    for i in range(len(database)):
-        one_record = database[i].split(";")
+    flag_found: bool = False
+    for index in range(len(database)):
+        one_record = database[index].split(";")
         id_number = (int(one_record[0]))
         last_name = (str(one_record[1]))
         first_name = (str(one_record[2]))
@@ -18,9 +20,38 @@ def find_record_in(database, what_im_looking_for: str):
 
         if what_im_looking_for in one_record:
             # print(one_record)
-            print(f"ID: {id_number}, \t Фамилия: {last_name}, \tИмя: {first_name}, \t Отчество: {patronymic}, \t\tТелефон: {phone_num};")
+            flag_found = True
+            print(
+                f"ID: {id_number}, \t Фамилия: {last_name}, \tИмя: {first_name}, \t Отчество: {patronymic}, \t\tТелефон: {phone_num};")
+    return flag_found
 
 
 # find_record_in(database, "Конев")
 # find_record_in(database, "Ивано")
 # find_record_in(database, "Ингви")
+
+
+def delete_existing_contact_from(file_database):
+    id_number = int(input("Введите ID номер записи для ее удаления: "))
+    memory = list()
+    with (open(file_database, 'r', encoding="utf-8")) as db:
+        for line in db:
+            memory.append(line.strip())
+    memory.pop(id_number - 1)
+
+    for index in range(len(memory)):
+        one_record = memory[index].split(";")
+        id_number = (int(one_record[0]))
+        last_name = (str(one_record[1]))
+        first_name = (str(one_record[2]))
+        patronymic = (str(one_record[3]))
+        phone_num = (str(one_record[4]))
+
+    with open(file_database, "w+", encoding="utf-8") as handle:
+        for index in range(len(memory)):
+            one_record = memory[index]
+            # print(one_record)
+            handle.write(one_record + "\n")
+
+
+# delete_existing_contact_from(database)
--- Course14(PythonBegin)/gb.ru-lessons-328478-homework/Задача38/Задача38.py
+++ Course14(PythonBegin)/gb.ru-lessons-328478-homework/Задача38/Задача38.py
@@ -1,11 +1,12 @@
 from core import find_record_in
+from core import delete_existing_contact_from
 
-database: str = "database.txt"
+file_database: str = "database.txt"
 
 
 def show_all_contacts():
     memory = list()
-    with (open(database, 'r', encoding="utf-8")) as db:
+    with (open(file_database, 'r', encoding="utf-8")) as db:
         for line in db:
             data = line.replace(";", " ")
             memory.append(line.strip())
@@ -16,37 +17,33 @@ def show_all_contacts():
 
 def get_size_of_database():
     memory = list()
-    with (open(database, 'r', encoding="utf-8")) as db:
+    with (open(file_database, 'r', encoding="utf-8")) as db:
         for line in db:
             memory.append(line.strip())
-    print()
+    # print()
     # print(len(memory))
     return len(memory)
 
 
 def get_virtual_database() -> list:
     memory = list()
-    with (open(database, 'r', encoding="utf-8")) as db:
+    with (open(file_database, 'r', encoding="utf-8")) as db:
         for line in db:
             memory.append(line.strip())
     return memory
 
 
-def search_record():
-    pass
-
-
 def add_new_contact():
     id_number = get_size_of_database() + 1
 
-    firstname = input("Введите Имя: ")
-    last_name = input("Введите Фамилию: ")
-    patronymic = input("Введите Отчество: ")
-    phone_number = input("Введите номер телефона: ")
+    firstname = input("Введите Имя: ") or " - "
+    last_name = input("Введите Фамилию: ") or " - "
+    patronymic = input("Введите Отчество: ") or " - "
+    phone_number = input("Введите номер телефона: ") or " - "
     splitter = str(";")
     new_record = str(
         id_number) + splitter + firstname.strip() + splitter + last_name.strip() + splitter + patronymic.strip() + splitter + phone_number.strip()
-    with open(database, "a", encoding="utf-8") as db:
+    with open(file_database, "a", encoding="utf-8") as db:
         db.write(new_record + "\n")
 
 
@@ -72,13 +69,14 @@ def add_new_contact():
 
 
 def main():
-    # print(get_virtual_database())
+    print(get_virtual_database())
 
     print("ТЕЛЕФОННЫЙ СПРАВОЧНИК")
     print("Выберите действие: \n"
           "1 - Показать все контакты; \n"
           "2 - Найти контакт; \n"
           "3 - Добавить контакт: \n"
+          "4 - Удалить контакт: \n"
           "Введите цифру и нажмите Enter: ")
 
     user_selected_is = int(input())
@@ -86,10 +84,15 @@ def main():
         show_all_contacts()
 
     if user_selected_is == 2:
-        find_record_in(get_virtual_database(), str(input("Введите что ищем: ")))
+        flag_found = find_record_in(get_virtual_database(), str(input("Введите что ищем: ")))
+        if flag_found == False:
+            print("По вашему запросу нет информации в базе.")
 
     if user_selected_is == 3:
         add_new_contact()
+
+    if user_selected_is == 4:
+        delete_existing_contact_from(file_database)
 
 
 main()
