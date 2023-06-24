a827cc24264784bd4a7a089110ff5d6647913200
DANIILKONEF <daniilkonef@yandex.ru>
2023-06-24 13:33:21 +0300

    Почти готов метод find_record_in

---
+++ Course14(PythonBegin)/gb.ru-lessons-328478-homework/Задача38/Задача38.py
@@ -0,0 +1,90 @@
+database: str = "database.txt"
+
+
+def show_all_contacts():
+    memory = list()
+    with (open(database, 'r', encoding="utf-8")) as db:
+        for line in db:
+            data = line.replace(";", " ")
+            memory.append(line.strip())
+            print(data, end="")
+    # print()
+    # print(len(memory))
+
+
+def get_size_of_database():
+    memory = list()
+    with (open(database, 'r', encoding="utf-8")) as db:
+        for line in db:
+            memory.append(line.strip())
+    print()
+    # print(len(memory))
+    return len(memory)
+
+
+def get_virtual_database() -> list:
+    memory = list()
+    with (open(database, 'r', encoding="utf-8")) as db:
+        for line in db:
+            memory.append(line.strip())
+    return memory
+
+
+def search_record():
+    pass
+
+
+def add_new_contact():
+    id_number = get_size_of_database() + 1
+
+    firstname = input("Введите Имя: ")
+    last_name = input("Введите Фамилию: ")
+    patronymic = input("Введите Отчество: ")
+    phone_number = input("Введите номер телефона: ")
+    splitter = str(";")
+    new_record = str(id_number) + splitter + firstname.strip() + splitter + last_name.strip() + splitter + patronymic.strip() + splitter + phone_number.strip()
+    with open(database, "a", encoding="utf-8") as db:
+        db.write(new_record + "\n")
+
+
+# def show_all_records():
+#     with open(database, 'r', encoding='utf-8') as f:
+#         for line in f:
+#             print(*line.strip().split(';'))
+
+
+# def search_record(num_col, val):
+#     with open(file_path, 'r', encoding='utf-8') as f:
+#         for line in f:
+#             lst = line.strip().split(';')
+#             if lst[num_col - 1].lower() == val.lower():
+#                 print(*lst)
+#                 return
+#     print(f'{val} не нашел')
+
+
+# def add_contact(lst):
+#     with open(file_path, 'a', encoding='utf-8') as f:
+#         print(';'.join(lst), file=f)
+
+
+def main():
+    print(get_virtual_database())
+
+    print("ТЕЛЕФОННЫЙ СПРАВОЧНИК")
+    print("Выберите действие: \n"           
+          "1 - Показать все контакты; \n"           
+          "2 - Найти контакт; \n"          
+          "3 - Добавить контакт: \n"          
+          "введите цифру и нажмите Enter: ")
+
+    user_selected = int(input())
+    if user_selected == 1:
+        show_all_contacts()
+    if user_selected == 2:
+        search_record()
+    if user_selected == 3:
+        add_new_contact()
+
+
+main()
