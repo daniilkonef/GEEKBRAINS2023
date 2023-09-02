25f11c755367e0d9814f65443be893f9d0e2d085
DANIILKONEF <daniilkonef@yandex.ru>
2023-09-02 23:33:10 +0300

    Задча1 решена

---
+++ Course20(Exceptions)/Seminar2/Task1/src/main/java/daniilkonef/Program.java
@@ -0,0 +1,33 @@
+package daniilkonef;
+
+import java.util.Scanner;
+//import static Printer.*;
+import static daniilkonef.Printer.printLine;
+
+
+
+
+public class Program {
+    public static void main(String[] args) {
+
+        while (true)
+        {
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("Введите ваше дробное число: ");
+            String line = input.nextLine(); //System.out.println(query);
+            if (line.equalsIgnoreCase(new String("exit"))) break;
+            try {
+                float number = Float.parseFloat(line);
+            }
+            catch (NumberFormatException ex){
+                printLine("вы ввели ошибочное число, просьба попробовать снова.");
+                continue;
+
+            }
+
+
+        }
+
+    }
+}
